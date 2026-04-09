659dc670819d7149b8ce1cdd52953b4066d5cddb
marcelod64 <167905577+marcelod6427@users.noreply.github.com>
2026-04-08 22:10:51 -0300

    Add files via upload

---
+++ semana-8/scrips/script3.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+#Definição de variaveis e atribuição
+autor="Davi P. Conceição"
+data=$(data +%x)
+# O comnado entre $() será executada e
+# seu resultado guardado na variavel
+echo"Script desenvolvido por $autor"
+echo"Data de criação: $data"
