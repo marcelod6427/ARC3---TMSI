68cea2153473db74a7909bf5966a5d86ddcdc6a7
marcelod64 <167905577+marcelod6427@users.noreply.github.com>
2026-04-23 01:12:51 +0000

    Delete semana-8 directory

--- semana-8/exemplo_if.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-if [ $USER == "root" ]
-then
-    echo"Acesso total permitido"
-
-else
-    echo" Voçe precisa de privilegios de root"
--- semana-8/internet_check.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-clear
-ping  -c 3 8.8.8.8 &> /dev/null
-
-if [ $? -eq 0 ]
-then
-echo"Conexão com internet ok"
-fi
